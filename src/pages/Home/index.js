9dcd98b5c0093e21084a077ac937909e675d588c
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-30 18:28:11 -0300

    Add JSONServer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,106 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/04/D22-2763-004/D22-2763-004_zoom1.jpg"
-          alt="Tênis Olympikus Easy Feminino - Bege"
-        />
-        <strong>Tênis Olympikus Easy Feminino - Bege</strong>
-        <span>R$169,90</span>
+export default class Home extends Component {
+  state = {
+    products: []
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/04/D22-2763-004/D22-2763-004_zoom1.jpg"
-          alt="Tênis Olympikus Easy Feminino - Bege"
-        />
-        <strong>Tênis Olympikus Easy Feminino - Bege</strong>
-        <span>R$169,90</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+    this.setState({ products: data });
+  }
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/04/D22-2763-004/D22-2763-004_zoom1.jpg"
-          alt="Tênis Olympikus Easy Feminino - Bege"
-        />
-        <strong>Tênis Olympikus Easy Feminino - Bege</strong>
-        <span>R$169,90</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/04/D22-2763-004/D22-2763-004_zoom1.jpg"
-          alt="Tênis Olympikus Easy Feminino - Bege"
-        />
-        <strong>Tênis Olympikus Easy Feminino - Bege</strong>
-        <span>R$169,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/04/D22-2763-004/D22-2763-004_zoom1.jpg"
-          alt="Tênis Olympikus Easy Feminino - Bege"
-        />
-        <strong>Tênis Olympikus Easy Feminino - Bege</strong>
-        <span>R$169,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-olympikus-easy-feminino/04/D22-2763-004/D22-2763-004_detalhe1.jpg?resize=280:280"
-          alt="Tênis Olympikus Easy Feminino - Bege"
-        />
-        <strong>Tênis Olympikus Easy Feminino - Bege</strong>
-        <span>R$169,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />1
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />1
+              </div>
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
